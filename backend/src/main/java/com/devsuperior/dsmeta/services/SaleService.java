e97d95ce5904fe055c2a544a6ec2172d01f9829b
André Igor <andreigormenezes@gmail.com>
2022-11-18 16:06:27 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,20 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoField;
-
+import com.devsuperior.dsmeta.model.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dsmeta.model.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
