214f49507708093aae21ea066f68cc623146aaef
André Igor <andreigormenezes@gmail.com>
2022-11-18 12:47:02 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,20 +18,20 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 public class SaleService {
-    
+
     @Autowired
     private SaleRepository saleRepository;
 
     @Transactional
     public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-	
-	LocalDate min = LocalDate.parse(minDate);
-	LocalDate max = LocalDate.parse(maxDate);
-	
-	
-	return saleRepository.findSales(min, max, pageable);
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return saleRepository.findSales(min, max, pageable);
     }
-    
-    
-    
+
+
 }
