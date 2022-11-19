119bbe2dfe89eb524db1a7cd46415e54f1f8d1e8
André Igor <andreigormenezes@gmail.com>
2022-11-19 12:53:41 -0300

    Corrections

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsmeta.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,16 +1,17 @@
 package com.devsuperior.dsmeta.services;
 
-import com.devsuperior.dsmeta.model.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
+import com.devsuperior.dsmeta.model.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
